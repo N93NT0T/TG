cfa38f5aebc001008a63ad7e1907f5140291cdef
N93NT0T <75997866+N93NT0T@users.noreply.github.com>
2022-03-08 10:27:43 +0700

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -27,8 +27,8 @@ async def gen_link_s(bot, message):
     if not replied:
         return await message.reply('Reply to a message to get a shareable link.')
     file_type = replied.media
-    if file_type not in ["video", 'audio', 'document']:
-        return await message.reply("Reply to a supported media")
+   # if file_type not in ["video", 'audio', 'document']:
+  #      return await message.reply("Reply to a supported media")
     if message.has_protected_content and message.chat.id not in ADMINS:
         return await message.reply("Protect by self")
     file_id, ref = unpack_new_file_id((getattr(replied, file_type)).file_id)
